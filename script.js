e5fb582c32bf5b305f7c5b107607e318958c7d64
jashwanthreddy2508-commits <jashwanthreddy2508@gmail.com>
2026-09-04 22:38:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function validate() {
+
+let text=document.getElementById("text").value;
+let delay=document.getElementById("delay").value;
+let output=document.getElementById("output");
+
+let promise=new Promise((resolve,reject)=>{
+
+	setTimeout(()=>{
+		resolve(text)
+	},delay)
+})
+async function result() {
+	let data=await promise;
+	output.innerHTML+=`
+	<p>${data}</p>
+	`
+}
+	result();
+}
